e98b6b2b80399759aacf697d6f4bbd83369094e8
Peter Engelbert <pmengelbert@gmail.com>
2019-10-24 17:06:56 -0500

    restructured directories

---
+++ classes/user_library.rb
@@ -0,0 +1,37 @@
+require 'json'
+
+class UserLibrary
+  include Enumerable
+
+  attr_accessor :books, :filename
+
+  def initialize(sourcefile = nil)
+    if sourcefile
+      @filename = File.absolute_path(sourcefile)
+      file = File.open(@filename)
+      @books = JSON.parse(file.read)
+    else
+      @filename = File.absolute_path("saved_libraries/library.json")
+      @books = []
+    end
+  end
+
+  def add(book)
+    @books << book
+  end
+
+  def save
+    File.write(@filename, to_json)
+  end
+
+  def each
+    @books.each { |b| yield(b) }
+  end
+
+  def to_json
+    a = @books.map { |b| b.info }
+    JSON.pretty_generate(a)
+  end
+
+end
+
--- g_search.rb
+++ g_search.rb
@@ -1,41 +1,7 @@
 require 'json'
 require_relative 'common/api_query'
 require_relative 'common/google_api_key'
-require_relative 'classes/user_book'
 require_relative 'classes/book_search'
-
-class UserLibrary
-  include Enumerable
-
-  attr_accessor :books, :filename
-
-  def initialize(sourcefile = nil)
-    if sourcefile
-      @filename = File.absolute_path(sourcefile)
-      file = File.open(@filename)
-      @books = JSON.parse(file.read)
-    else
-      @filename = File.absolute_path("library.json")
-      @books = []
-    end
-  end
-
-  def add(book)
-    @books << book
-  end
-
-  def save
-    File.write(@filename, to_json)
-  end
-
-  def each
-    @books.each { |b| yield(b) }
-  end
-
-  def to_json
-    a = @books.map { |b| b.info }
-    JSON.pretty_generate(a)
-  end
-
-end
+require_relative 'classes/user_book'
+require_relative 'classes/user_library'
 
