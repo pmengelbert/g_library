7ed4ae976bc6fc7dda67c8c8eb983796918c5601
Peter Engelbert <pmengelbert@gmail.com>
2019-10-24 12:45:14 -0500

    Initial commit

---
+++ common/api_query.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+
+module Curl
+  require 'net/http'
+  require 'json'
+
+  private
+    def get_response(url)
+      Net::HTTP.get_response(URI.parse(url)).body
+    end
+
+    def get_response_hash(url)
+      JSON.parse(get_response(url))
+    end
+end
+
