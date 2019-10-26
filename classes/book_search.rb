6839b9bb92168c2413b5cd95b38dd1522ec90fdc
Peter Engelbert <pmengelbert@gmail.com>
2019-10-25 22:40:26 -0500

    commented g_library.rb and classes/book_search.rb

--- classes/book_search.rb
+++ classes/book_search.rb
@@ -11,34 +11,51 @@ class BookSearch
 
   def initialize(args)
     @args = args.dup
+    #
+    #the number of results to show:
     num_results = @args.delete(:num) || 5
 
+    #make sure at least one argument is searchable
     raise ArgumentError unless args.keys.any? do |k| 
-      %i[title author publisher subject isbn lccn oclc].include?(k)
+      %i[search title author publisher subject isbn lccn oclc].include?(k)
     end
 
-    @args = args.map { |k, v| [k,v].map(&:to_s) }.to_h
+    #map the keys and values to strings for ease of manipulation
+    @args = @args.map { |k, v| [k,v].map(&:to_s) }.to_h
+    #remove any empty values (nil values were converted to 
+    #empty strings by the last operation)
     @args = @args.reject { |k, v| v.empty? }.to_h
 
+    #get a nicely formatted url; very important
     @url = make_url
 
+    #check for successful response from the API
     response = get_response(url)
     raise SearchError unless response.code == "200"
 
+    #Puts the results into a hash
     @full_results = JSON.parse(response.body)
 
     num = full_results['totalItems']
     raise NoResults unless num > 0
 
-    @selected_results = full_results['items'].first(num_results).map { |res| format_hash res }
+    #the selected results:
+    @selected_results = full_results['items'].first(num_results)
+    #The following mapping gets the 'volumeInfo' property from the results,
+    #and adds the 'id' property.
+    @selected_results = @selected_results.map { |res| format_hash res }
 
   end
 
+  #Only five results are iterated over; This behavior is easily changed by either
+  #passing a :num value when initializing a BookSearch, or by using full_results
+  #below, instead of selected_results
   def each
     return to_enum :each unless block_given?
     selected_results.each { |r| yield(r) }
   end
 
+  #Likewise, only five results are accessible here; see the previous note.
   def [](index)
     selected_results[index]
   end
@@ -48,6 +65,7 @@ class BookSearch
   end
 
   private 
+
     def make_url_arg_list
       url = ["?q="]
 
@@ -60,19 +78,22 @@ class BookSearch
       end
 
       url.reject!(&:empty?)
-
       url[0] + url[1, url.length-1].join("+")
-
     end
 
+
+    #gets the 'volumeInfo' property from the results, and adds the 'id' property.
     def format_hash(hash)
       hash['volumeInfo'].merge( { 'id' => hash['id'] } )
     end
 
+    #Add the base url and the formatted arg list together, and make sure
+    #there are no spaces
     def make_url
       (BASE_API_URL + make_url_arg_list).gsub(/ /, "+")
     end
 
+    #for testing purposes only
     def args
       @args
     end
--- g_library.rb
+++ g_library.rb
@@ -12,16 +12,19 @@ require_relative 'common/errors'
 
 ARGV << '-h' if ARGV.empty?
 
-if ARGV.include?("-l")
-  ARGV << ARGV.delete("-l")
-end
+#make sure the -l flag is process *after* the -f flag, by putting it at the end
+ARGV << ARGV.delete("-l") if ARGV.include?("-l")
 
+#default filename for saved reading list
 filename = File.dirname(File.expand_path(__FILE__)) + "/saved_libraries/library.json"
 
+#initialize the hash which will hold the search parameters
 o = {}
 
+#parse the command-line options
 OptionParser.new do |opts|
   opts.banner = "Usage: ruby g_library.rb [options...] [query]"
+  #sets 'library-mode'
   library = opts.default_argv.include?("-l")
 
   opts.on("-t", "--title=TITLE", "Specify a title keyword") do |t|
@@ -45,8 +48,13 @@ OptionParser.new do |opts|
   end
 
   opts.on("-l", "--library", "See your library; ignores all search options
-          \n\t\t\t\t\tdefault library file is [repository_root]/saved_libraries/library.json") do
-    filename.gsub!(/\//, '\\') if ENV.values.any? { |v| v =~ /C:\\Windows/i }
+          \t\t\t\t\tdefault library file is [repository_root]/saved_libraries/library.json") do
+
+    #substitute \ for / if the machine is running windows
+    filename.gsub!(/\//, '\\') if ENV.values.any? { |v| v =~ /[A-Z]:\\Windows/i }
+
+
+    #print out the library and exit the program
     l = UserLibrary.new(filename)
     l.pretty_print
     exit
@@ -62,14 +70,23 @@ OptionParser.new do |opts|
 
 end.parse!
 
+#substitute \ for / if the machine is running windows
 filename.gsub!(/\//, '\\') if ENV.values.any? { |v| v =~ /C:\\Windows/i }
+
+# Create or load the library, depending on whether or not the file already exists.
 l = UserLibrary.new(filename)
 
-
+#initialize the 
+#perform the search, and display the results.
 begin
   s = BookSearch.new(search: ARGV.join('+'), title: o[:title], author: o[:author], publisher: o[:publisher])
+
+  #see BookSearch definition in classes/book_search.rb
+  #the each method (and all Enumerable methods, like map) only operate on
+  #the first five matches.
   books = s.map { |info| UserBook.new(info) }
 
+  #print the resuls
   puts ""
   books.each_with_index do |b, i|
     keys = %w[title author publisher]
@@ -80,6 +97,18 @@ begin
     end
     puts ""
   end
+
+  print "Enter a number (1-5) to add a book to your reading list:\n(or any other key to quit)\n"
+
+  selection = STDIN.getch.chomp.to_i
+  i = selection - 1
+  puts ""
+  exit if i < 0 or i > 4
+
+  #add a book to the user's library
+  l.add(books[i])
+  l.save
+
 rescue SearchError 
   puts "\nThere was an error with your query; be careful to format it well"
   puts ""
@@ -89,13 +118,3 @@ rescue NoResults
   puts ""
   exit
 end
-
-print "Enter a number (1-5) to add a book to your reading list:\n(or any other key to quit)\n"
-
-selection = STDIN.getch.chomp.to_i
-i = selection - 1
-puts ""
-exit if i < 0 or i > 4
-
-l.add(UserBook.new(s[i]))
-l.save
