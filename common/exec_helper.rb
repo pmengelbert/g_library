aab6bab8a15d4892597f10485940cec7c6df6dfb
Peter Engelbert <pmengelbert@gmail.com>
2019-11-09 16:27:11 -0600

    Wrapped common/exec_helper.rb methods in ExecHelper module

--- common/exec_helper.rb
+++ common/exec_helper.rb
@@ -1,13 +1,15 @@
-def process_args!(args = ARGV)
-  args << '-h' if args.empty?
-  args << args.delete("-l") if args.include?("-l")
-end
+module ExecHelper
+  def process_args!(args = ARGV)
+    args << '-h' if args.empty?
+    args << args.delete("-l") if args.include?("-l")
+  end
 
-def prepare_filename_for_os!(filename)
-  filename.gsub!(/\//, '\\') if ENV.values.any? { |v| v =~ /[A-Z]:\\Windows/i }
-end
+  def prepare_filename_for_os!(filename)
+    filename.gsub!(/\//, '\\') if ENV.values.any? { |v| v =~ /[A-Z]:\\Windows/i }
+  end
 
-def perform_search(o)
-  s = BookSearch.new(search: ARGV.join('+'), title: o[:title], author: o[:author], 
-                     publisher: o[:publisher])
+  def perform_search(o)
+    s = BookSearch.new(search: ARGV.join('+'), title: o[:title], author: o[:author], 
+                       publisher: o[:publisher])
+  end
 end
